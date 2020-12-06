7ab6626e11f1833675260acfe48024e460d352b1
Haluk Yıldız <halukyildiz@Haluk-MacBook-Air.local>
2020-12-06 21:10:55 +0300

    seo favicon removed

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,10 +4,10 @@ import { Helmet } from "react-helmet"
 import { useLocation } from "@reach/router"
 import { useStaticQuery, graphql } from "gatsby"
 
-import favicon16 from "../images/favicon16.png";
-import favicon32 from "../images/favicon32.png";
-import favicon64 from "../images/favicon64.png";
-import appletouchicon from "../images/appletouchicon.png";
+// import favicon16 from "../images/favicon16.png";
+// import favicon32 from "../images/favicon32.png";
+// import favicon64 from "../images/favicon64.png";
+// import appletouchicon from "../images/appletouchicon.png";
 
 const SEO = ({ title, description, image, article }) => {
   const { pathname } = useLocation()
@@ -30,16 +30,18 @@ const SEO = ({ title, description, image, article }) => {
   }
 
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate} link={[
-    { rel: "icon", type: "image/png", sizes: "16x16", href: `${favicon16}` },
-    { rel: "icon", type: "image/png", sizes: "32x32", href: `${favicon32}` },
-    { rel: "shortcut icon", type: "image/png",sizes: "64x64", href: `${favicon64}` },
-    { rel: "apple-touch-icon", type: "image/png", sizes: "57x57",  href: `${appletouchicon}` },
-  ]} >
+    <Helmet title={seo.title} titleTemplate={titleTemplate} 
+    // link={[
+  //   { rel: "icon", type: "image/png", sizes: "16x16", href: `${favicon16}` },
+  //   { rel: "icon", type: "image/png", sizes: "32x32", href: `${favicon32}` },
+  //   { rel: "shortcut icon", type: "image/png",sizes: "64x64", href: `${favicon64}` },
+  //   { rel: "apple-touch-icon", type: "image/png", sizes: "57x57",  href: `${appletouchicon}` },
+  // ]} 
+  >
       <html lang="tr" />
       <meta name="description" content={seo.description} lang="tr" />
       <meta name="image" content={seo.image} />
-         <link
+         {/* <link
           rel="apple-touch-icon"
           sizes="57x57"
           href="../images/appletouchicon.png"
@@ -55,7 +57,7 @@ const SEO = ({ title, description, image, article }) => {
           type="image/png"
           sizes="64x64"
           href="../images/favicon16.png"
-        />
+        /> */}
       
 
       {seo.url && <meta property="og:url" content={seo.url} />}
