5970245b1f81b79d1232dee1e4e6c85c07665752
Haluk Yıldız <halukyildiz@Haluk-MacBook-Air.local>
2020-12-06 22:09:14 +0300

    favicons added by helmet

--- src/components/seo.js
+++ src/components/seo.js
@@ -41,6 +41,24 @@ const SEO = ({ title, description, image, article }) => {
       <html lang="tr" />
       <meta name="description" content={seo.description} lang="tr" />
       <meta name="image" content={seo.image} />
+      <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
          {/* <link
           rel="apple-touch-icon"
           sizes="57x57"
